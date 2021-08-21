575fc988f350f05846ae338d025938d6aaa61bd7
Savvy Hobbit <>
2021-08-21 13:47:34 -0400

    Initial code rebuild

--- 11.bundle.js
+++ 11.bundle.js
@@ -1,39 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{107:function(e,t,a){"use strict";a.d(t,"b",(function(){return d})),a.d(t,"a",(function(){return n})),a.d(t,"c",(function(){return c}));const i=document.createElement("div");let s=!1;function d(){return i}function n(e){i.dispatchEvent(new CustomEvent("editModeChange",{bubbles:!0,composed:!0,detail:{isEditMode:e}}))}function c(){return s}i.addEventListener("editModeChange",e=>{e.detail&&(s=!!e.detail.isEditMode)})},120:function(e,t,a){"use strict";var i=a(7),s=a(104);class d extends i.a{static get properties(){return{initialValue:{type:Boolean,value:!1,observer:"initValueChange"},checked:{type:Boolean,value:!1,reflectToAttribute:!0},label:{type:String,value:""},secondaryLabel:{type:String,value:""},disabled:{type:Boolean,value:!1,reflectToAttribute:!0}}}initValueChange(){this.switchEl&&(this.switchEl.checked=this.initialValue,this.checked=this.initialValue)}ready(){super.ready(),setTimeout(()=>{this.switchEl=new s.a(this.shadowRoot.querySelector(".mdc-switch")),this.switchEl.checked=this.initialValue,this.checked=this.initialValue},10)}connectedCallback(){super.connectedCallback(),this.switchEventHandler=()=>{this.checked=this.switchEl.checked,this.dispatchEvent(new CustomEvent("switch-change",{detail:{checked:this.switchEl.checked},bubbles:!0,composed:!0}))},this.shadowRoot.querySelector(".mdc-switch__native-control").addEventListener("change",this.switchEventHandler)}disconnectedCallback(){super.disconnectedCallback(),this.shadowRoot.querySelector(".mdc-switch__native-control").removeEventListener("change",this.switchEventHandler)}_switchClasses(e){return e?"mdc-switch mdc-list-item__meta mdc-switch--disabled":"mdc-switch mdc-list-item__meta"}static get template(){return i.b`
-      <style include="material-styles">
-        :host {
-          display: inline-block;
-        }
-        :host([checked]) label.secondary {
-          color: var(--mdc-theme-primary);
-        }
-        :host([checked]) label:not(.secondary) {
-          color: var(--lumo-secondary-text-color);
-        }
-        label {
-          color: var(--mdc-theme-primary);
-          font-weight: 500;
-          font-size: var(--lumo-font-size-s);
-          margin-right: 16px;
-          transition: color 0.2s;
-        }
-        label.secondary {
-          color: var(--lumo-secondary-text-color);
-          margin-right: 0;
-          margin-left: 16px;
-        }
-      </style>
-      
-      <label for="swich">[[label]]</label>
-      <div class$="[[_switchClasses(disabled)]]">
-        <div class="mdc-switch__track"></div>
-        <div class="mdc-switch__thumb-underlay">
-          <div class="mdc-switch__thumb">
-            <input type="checkbox" id="swich" class="mdc-switch__native-control" role="switch" disabled$="[[disabled]]" />
-          </div>
-        </div>
-      </div>
-      <label class="secondary">[[secondaryLabel]]</label>
-    `}}customElements.define("dnd-switch",d)},141:function(e,t,a){var i={"./dnd-character-builder-attributes":[124,4,8],"./dnd-character-builder-attributes.js":[124,4,8],"./dnd-character-builder-background-race":[121,9],"./dnd-character-builder-background-race.js":[121,9],"./dnd-character-builder-class":[128,3,4,5],"./dnd-character-builder-class.js":[128,3,4,5],"./dnd-character-builder-equipment":[125,3,6],"./dnd-character-builder-equipment.js":[125,3,6],"./dnd-character-builder-spells":[126,3,7],"./dnd-character-builder-spells.js":[126,3,7]};function s(e){if(!a.o(i,e))return Promise.resolve().then((function(){var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}));var t=i[e],s=t[0];return Promise.all(t.slice(1).map(a.e)).then((function(){return a(s)}))}s.keys=function(){return Object.keys(i)},s.id=141,e.exports=s},99:function(e,t,a){"use strict";a.r(t);var i=a(7),s=(a(62),a(70),a(144));a(74),a(75);class d extends i.a{static get properties(){return{tabs:{type:Array,observer:"tabsChanged"},initialSelectedIndex:{type:Number,value:0}}}tabsChanged(){this.tabs.length&&setTimeout(()=>{this.tabBar=new s.a(this.$.tabs),this.$.tabs.addEventListener("MDCTabBar:activated",e=>{this.handleTabChange(e.detail.index)}),this.tabBar.activateTab(this.initialSelectedIndex)},0)}handleTabChange(e){this.dispatchEvent(new CustomEvent("tabChange",{bubbles:!0,composed:!0,detail:{index:e}}))}static get template(){return i.b`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{100:function(e,t,a){"use strict";a.r(t);var i=a(7),s=(a(62),a(70),a(144));a(74),a(75);class d extends i.a{static get properties(){return{tabs:{type:Array,observer:"tabsChanged"},initialSelectedIndex:{type:Number,value:0}}}tabsChanged(){this.tabs.length&&setTimeout(()=>{this.tabBar=new s.a(this.$.tabs),this.$.tabs.addEventListener("MDCTabBar:activated",e=>{this.handleTabChange(e.detail.index)}),this.tabBar.activateTab(this.initialSelectedIndex)},0)}handleTabChange(e){this.dispatchEvent(new CustomEvent("tabChange",{bubbles:!0,composed:!0,detail:{index:e}}))}static get template(){return i.b`
       <style include="material-styles">
         .mdc-tab-bar {
           max-width: 100vw;
@@ -233,5 +198,40 @@
           </div>
         </div>
       </div>
-    `}}customElements.define("dnd-character-builder-view",o)}}]);
+    `}}customElements.define("dnd-character-builder-view",o)},107:function(e,t,a){"use strict";a.d(t,"b",(function(){return d})),a.d(t,"a",(function(){return n})),a.d(t,"c",(function(){return c}));const i=document.createElement("div");let s=!1;function d(){return i}function n(e){i.dispatchEvent(new CustomEvent("editModeChange",{bubbles:!0,composed:!0,detail:{isEditMode:e}}))}function c(){return s}i.addEventListener("editModeChange",e=>{e.detail&&(s=!!e.detail.isEditMode)})},120:function(e,t,a){"use strict";var i=a(7),s=a(104);class d extends i.a{static get properties(){return{initialValue:{type:Boolean,value:!1,observer:"initValueChange"},checked:{type:Boolean,value:!1,reflectToAttribute:!0},label:{type:String,value:""},secondaryLabel:{type:String,value:""},disabled:{type:Boolean,value:!1,reflectToAttribute:!0}}}initValueChange(){this.switchEl&&(this.switchEl.checked=this.initialValue,this.checked=this.initialValue)}ready(){super.ready(),setTimeout(()=>{this.switchEl=new s.a(this.shadowRoot.querySelector(".mdc-switch")),this.switchEl.checked=this.initialValue,this.checked=this.initialValue},10)}connectedCallback(){super.connectedCallback(),this.switchEventHandler=()=>{this.checked=this.switchEl.checked,this.dispatchEvent(new CustomEvent("switch-change",{detail:{checked:this.switchEl.checked},bubbles:!0,composed:!0}))},this.shadowRoot.querySelector(".mdc-switch__native-control").addEventListener("change",this.switchEventHandler)}disconnectedCallback(){super.disconnectedCallback(),this.shadowRoot.querySelector(".mdc-switch__native-control").removeEventListener("change",this.switchEventHandler)}_switchClasses(e){return e?"mdc-switch mdc-list-item__meta mdc-switch--disabled":"mdc-switch mdc-list-item__meta"}static get template(){return i.b`
+      <style include="material-styles">
+        :host {
+          display: inline-block;
+        }
+        :host([checked]) label.secondary {
+          color: var(--mdc-theme-primary);
+        }
+        :host([checked]) label:not(.secondary) {
+          color: var(--lumo-secondary-text-color);
+        }
+        label {
+          color: var(--mdc-theme-primary);
+          font-weight: 500;
+          font-size: var(--lumo-font-size-s);
+          margin-right: 16px;
+          transition: color 0.2s;
+        }
+        label.secondary {
+          color: var(--lumo-secondary-text-color);
+          margin-right: 0;
+          margin-left: 16px;
+        }
+      </style>
+      
+      <label for="swich">[[label]]</label>
+      <div class$="[[_switchClasses(disabled)]]">
+        <div class="mdc-switch__track"></div>
+        <div class="mdc-switch__thumb-underlay">
+          <div class="mdc-switch__thumb">
+            <input type="checkbox" id="swich" class="mdc-switch__native-control" role="switch" disabled$="[[disabled]]" />
+          </div>
+        </div>
+      </div>
+      <label class="secondary">[[secondaryLabel]]</label>
+    `}}customElements.define("dnd-switch",d)},141:function(e,t,a){var i={"./dnd-character-builder-attributes":[125,4,8],"./dnd-character-builder-attributes.js":[125,4,8],"./dnd-character-builder-background-race":[121,9],"./dnd-character-builder-background-race.js":[121,9],"./dnd-character-builder-class":[128,3,4,5],"./dnd-character-builder-class.js":[128,3,4,5],"./dnd-character-builder-equipment":[124,3,6],"./dnd-character-builder-equipment.js":[124,3,6],"./dnd-character-builder-spells":[126,3,7],"./dnd-character-builder-spells.js":[126,3,7]};function s(e){if(!a.o(i,e))return Promise.resolve().then((function(){var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}));var t=i[e],s=t[0];return Promise.all(t.slice(1).map(a.e)).then((function(){return a(s)}))}s.keys=function(){return Object.keys(i)},s.id=141,e.exports=s}}]);
 //# sourceMappingURL=11.bundle.js.map
